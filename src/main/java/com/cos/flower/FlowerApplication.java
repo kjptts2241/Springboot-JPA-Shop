fcd571541d1bf8b95e4876d264dc987144d4cece
Dongyeong Kim <kjptts2241@gmail.com>
2022-05-28 12:47:57 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/flower/FlowerApplication.java
@@ -0,0 +1,13 @@
+package com.cos.flower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerApplication.class, args);
+	}
+
+}
